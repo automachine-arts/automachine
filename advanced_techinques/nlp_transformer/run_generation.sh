653aaa2cc38c14d936e8e97eb2a7f0b0159fb0f5
orange.toaster <orange.toaster@gmail.com>
2020-01-12 15:08:11 -0500

    Added pytorch version.

--- advanced_techinques/nlp_transformer/run_generation.sh
+++ advanced_techinques/nlp_transformer/run_generation.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -exou pipefail
 
-python ./run_generation.py \
+# python ./pytorch_run_generation.py --model_type=gpt2 --length=20 --model_name_or_path=gpt2
+
+python ./pytorch_run_generation.py \
     --model_type=gpt2 \
     --length=20 \
     --model_name_or_path=gpt2 \
